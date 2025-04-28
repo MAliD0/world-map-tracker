d0d03e0ea6569a4689d64245c0dc819c8dd4e3fb
MAliD0 <mukalihan5@gmail.com>
2025-04-28 16:37:31 +0500

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// import "./Styles/globals.css";
-import "@/app/Styles/global.css"
+import "./Styles/globals.css";
+//import "@/app/Styles/global.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
