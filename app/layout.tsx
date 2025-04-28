0cdddc7c5a2f49450a9e916654b37854934744fc
MAliD0 <117840230+MAliD0@users.noreply.github.com>
2025-04-28 17:44:46 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./Styles/globals.css";
+import "./styles/globals.css";
 //import "@/app/Styles/global.css"
 
 const geistSans = Geist({
